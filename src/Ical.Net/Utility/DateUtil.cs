86b02cbf40f2fb135cb6fb0e31d1bb5363a386d7
skay97 <salmankarimzindani@hotmail.com>
2023-09-28 22:30:44 -0500

    feat: downgrades nodatime and uses appropriate namespace for xml serialization for time zone id

--- src/Ical.Net/Utility/DateUtil.cs
+++ src/Ical.Net/Utility/DateUtil.cs
@@ -122,7 +122,7 @@ namespace Ical.Net.Utility
                 return DateTimeZoneProviders.Tzdb.GetZoneOrNull(ianaZone);
             }
 
-            zone = NodaTime.Xml.XmlSerializationSettings.DateTimeZoneProvider.GetZoneOrNull(tzId);
+            zone = DateTimeZoneProviders.Serialization.GetZoneOrNull(tzId);
             if (zone != null)
             {
                 return zone;
@@ -130,7 +130,7 @@ namespace Ical.Net.Utility
 
             //US/Eastern is commonly represented as US-Eastern
             var newTzId = tzId.Replace("-", "/");
-            zone = NodaTime.Xml.XmlSerializationSettings.DateTimeZoneProvider.GetZoneOrNull(newTzId);
+            zone = DateTimeZoneProviders.Serialization.GetZoneOrNull(newTzId);
             if (zone != null)
             {
                 return zone;
@@ -149,10 +149,11 @@ namespace Ical.Net.Utility
                 return DateTimeZoneProviders.Tzdb.GetZoneOrNull(ianaZone);
             }
 
-            foreach (var providerId in NodaTime.Xml.XmlSerializationSettings.DateTimeZoneProvider.Ids.Where(tzId.Contains))
+            foreach (var providerId in DateTimeZoneProviders.Serialization.Ids.Where(tzId.Contains))
             {
-                return NodaTime.Xml.XmlSerializationSettings.DateTimeZoneProvider.GetZoneOrNull(providerId);
+                return DateTimeZoneProviders.Serialization.GetZoneOrNull(providerId);
             }
+
 
             if (useLocalIfNotFound)
             {
@@ -198,7 +199,7 @@ namespace Ical.Net.Utility
             return newZone;
         }
 
-        public static bool IsSerializationTimeZone(DateTimeZone zone) => NodaTime.Xml.XmlSerializationSettings.DateTimeZoneProvider.GetZoneOrNull(zone.Id) != null;
+        public static bool IsSerializationTimeZone(DateTimeZone zone) => DateTimeZoneProviders.Serialization.GetZoneOrNull(zone.Id) != null;
 
         /// <summary>
         /// Truncate to the specified TimeSpan's magnitude. For example, to truncate to the nearest second, use TimeSpan.FromSeconds(1)
@@ -217,7 +218,7 @@ namespace Ical.Net.Utility
             var offset = isExact
                 ? 0
                 : 1;
-            return (int) Math.Floor(d.Day / 7.0) + offset;
+            return (int)Math.Floor(d.Day / 7.0) + offset;
         }
     }
 }
